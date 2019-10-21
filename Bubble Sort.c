e766e1bd290da7c60b4c045c3c55092b4760b7ab
Shivam Gupta <shivam16121993@gmail.com>
2019-10-21 19:28:22 +0530

    Create Bubble Sort.c

---
+++ Bubble Sort.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+void bubble_sort(int a[],int n)
+{
+	int i,j;
+	for(i=0;i<(n-1);i++)
+		for(j=0;j<(n-1-i);j++)
+			if(a[j]>a[j+1])
+				swap(&a[j],&a[j+1]);
+}
+
+
+void swap(int *x,int *y)
+{
+	int temp;
+	temp=*y;
+	*y=*x;
+	*x=temp;
+}
+void print_sorted(int a[],int n)
+{
+	for(int i=0;i<n;i++)
+		printf("%d ",a[i]);
+}
+
+int main()
+{
+	int arr[]={20,30,55,24,32,8};
+	int size;
+
+	size=sizeof(arr)/sizeof(arr[0]);
+	bubble_sort(arr,size);
+	print_sorted(arr,size);
+	return 0;
+}
